5fba6a302f1001ca09583eebf117b09e42e18b42
chuongdn02 <chuongnguyendn02@gmail.com>
2022-09-12 22:59:06 +0700

    a

---
+++ editlistview/app/build/generated/source/buildConfig/debug/com/exemple/editlistview/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.exemple.editlistview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.exemple.editlistview";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
